2b25e990734d887f6b393fb4b8c1c86d13058988
budkina <anna@budkina.com>
2021-02-25 17:33:46 +0300

    add new options

--- README.md
+++ README.md
@@ -1,20 +1,38 @@
 Select random fragments for each record in fasta file
 
-usage: get_random_fragments.py [-h] [--fragment_len_min FRAGMENT_LEN_MIN] [--fragment_len_max FRAGMENT_LEN_MAX] [--fragments_per_record FRAGMENTS_PER_RECORD] [--output OUTPUT] [input]
+usage: get_random_fragments.py [-h] [--random_length] [--allow_duplicates] [--allow_n_content] [--length_list LENGTH_LIST [LENGTH_LIST ...]] [--fragment_length_min FRAGMENT_LENGTH_MIN]
+                               [--fragment_length_max FRAGMENT_LENGTH_MAX] [--fragments_per_record FRAGMENTS_PER_RECORD] --output OUTPUT
+                               [input]
 
 positional arguments:
 
-  input Input fasta file
-  
+  input                 Input fasta file
+
+
 optional arguments:
 
-  -h, --help  show this help message and exit
-  
-  --fragment_len_min FRAGMENT_LEN_MIN Minimum fragment length, default = 300
-  
-  --fragment_len_max FRAGMENT_LEN_MAX Maximum fragment length, default = 1000
-  
-  --fragments_per_record FRAGMENTS_PER_RECORD Number of random fragment for a single fasta record, default = 10
-  
-  --output OUTPUT Output fasta file
-  
+  -h, --help            show this help message and exit
+
+  --random_length       Generate fragments with random length, default: disabled
+
+  --allow_duplicates    Allow fragments duplicates, default: disabled
+
+  --allow_n_content     , default: disabled
+
+  --length_list LENGTH_LIST [LENGTH_LIST ...]
+
+                        List of fragment length values, used if --random_length is not defined, default=[]
+
+  --fragment_length_min FRAGMENT_LENGTH_MIN
+
+                        Minimum fragment length, --random_length is required, default = 300
+
+  --fragment_length_max FRAGMENT_LENGTH_MAX
+
+                        Maximum fragment length --random_length is required, default = 1000
+
+  --fragments_per_record FRAGMENTS_PER_RECORD
+
+                        Number of random fragment for a single fasta record, default = 10
+
+  --output OUTPUT       Output fasta file
--- get_random_fragments.py
+++ get_random_fragments.py
@@ -5,32 +5,56 @@ from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 
 parser = argparse.ArgumentParser()
-parser.add_argument("input", nargs='?', help="Input fasta file")
-parser.add_argument('--fragment_len_min', help='Minimum fragment length, default = 300', default=300)
-parser.add_argument('--fragment_len_max', help='Maximum fragment length, default = 1000', default=1000)
+parser.add_argument("input", help="Input fasta file")
+parser.add_argument('--random_length', action='store_true', help='Generate fragments with random length, default: disabled')
+parser.add_argument('--allow_duplicates', action='store_true', help='Allow fragments duplicates, default: disabled')
+parser.add_argument('--allow_n_content', action='store_true', help=', default: disabled')
+parser.add_argument('--length_list', help="List of fragment length values, used if --random_length is not defined, default=[]", nargs='+', default=[])
+parser.add_argument('--fragment_length_min', help='Minimum fragment length, --random_length is required, default = 300', default=300)
+parser.add_argument('--fragment_length_max', help='Maximum fragment length --random_length is required, default = 1000', default=1000)
 parser.add_argument('--fragments_per_record', help='Number of random fragment for a single fasta record, default = 10', default=10)
-parser.add_argument('--output', help='Output fasta file')
+parser.add_argument('--output', help='Output fasta file', required=True)
+
 args = parser.parse_args()
+
+if not args.random_length and not args.length_list:
+    print("Please provide --random_length option or a list of length values with --length_list")
+    exit()
+elif args.random_length and args.length_list: 
+    print("Please select --random_length or a predefined list of length values")
+    exit()
 
 fasta_filename = args.input
 output_fasta_filename = args.output
-fragment_len_min=int(args.fragment_len_min)
-fragment_len_max=int(args.fragment_len_max)
+length_list = [int(l) for l in args.length_list]
+
+if args.random_length:
+    fragment_length_min=int(args.fragment_length_min)
+else:
+    fragment_length_min = min(length_list)
+
+if args.random_length:
+    fragment_length_max=int(args.fragment_length_max)
+else:
+    fragment_length_max = max(length_list)
+
 fragments_per_record = int(args.fragments_per_record)
 
-if fragment_len_min>fragment_len_max:
-	print("fragment_len_min>fragment_len_max")
+if fragment_length_min>fragment_length_max:
+	print("fragment_len_min>fragment_length_max")
 	exit()
 	
 fasta_io = SeqIO.parse(fasta_filename, "fasta")
 
 fragments=[]
+n_bp_list = ['R','Y','S','W','K','M','B','D','H','V','N']
 
 for record in fasta_io:
     current_id = record.id
     genome=record.seq.upper()
     genome_length = len(str(genome))
     record_fragments=[]
+    str_record_fragments=[]
     i = 0
     iteration_limit = fragments_per_record*100
 
@@ -42,17 +66,25 @@ for record in fasta_io:
             
         # select fragment from random position with random length
         fragment_position = random.randint(0, genome_length-1)    
-        if genome_length - fragment_position < fragment_len_min:
+        if genome_length - fragment_position < fragment_length_min:
             continue
         
-        fragment_length = min(random.randint(fragment_len_min, fragment_len_max),genome_length - fragment_position)
+        if args.random_length:
+            fragment_length = min(random.randint(fragment_length_min, fragment_length_max),genome_length - fragment_position)
+        else:
+            fragment_length = length_list[random.randint(0,len(length_list)-1)]
+
         fragment = genome[fragment_position:fragment_position+fragment_length]
         
         # discard fragments with Ns
-        if 'N' in fragment: 
+        if not args.allow_n_content and any([bp in n_bp_list for bp in fragment]): 
             continue
         
+        if not args.allow_duplicates and (fragment in str_record_fragments):
+            continue
+
         record_fragments.append(SeqRecord(fragment, id=current_id+'_'+str(fragment_position),description = ''))
+        str_record_fragments.append(fragment)
     
     # append fragments to output
     fragments+=record_fragments
